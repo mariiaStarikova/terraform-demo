08e34b0993b93ffc739c9792dbe1d54e17765e66
Iurii Musiienko <Iurii_Musiienko@epam.com>
2018-08-02 15:33:37 +0300

    relocate terraform template

---
+++ aws_iurii_terraform.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+  
+resource "aws_instance" "test_VM" {
+  # Amazon Linux AMI 2017.03.1 (HVM)
+  ami           = "ami-7c4f7097"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "${var.vm_name}"
+  }
+}
+
+variable "vm_name" {
+  description = "Name for VM to be created"
+}
