bb8ce67bd4deb7a1e1933123021bc902634739ff
Iurii Musiienko <Iurii_Musiienko@epam.com>
2018-08-03 12:57:41 +0300

    updated template

--- aws_iurii_terraform.tf
+++ aws_iurii_terraform.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "test_VM" {
   # Amazon Linux AMI 2017.03.1 (HVM)
   ami           = "ami-7c4f7097"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags {
     Name = "${var.vm_name}"
